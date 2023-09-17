eda3bb89f75629040613ad60f50d592d9ad9821d
Jakub Przybysz <kuba.kubis@interia.pl>
2023-09-17 15:35:29 +0200

    fupdated depedencies

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -54,128 +54,129 @@ const HomeStyles = styled.div`
 `;
 
 export default function Home() {
-    const [songTitle, setSongTitle] = useState('');
-    const [artistName, setArtistName] = useState('');
-    const [lyrics, setLyrics] = useState('');
-    const [missingWords, setMissingWords] = useState([]);
-    const [answers, setAnswers] = useState([]);
-  
-    const fetchLyrics = async () => {
-      try {
-        const token = process.env.REACT_APP_GENIUS_TOKEN;
-        const response = await fetch(`https://api.genius.com/search?q=${songTitle} ${artistName}`, {
-          headers: {
-            'Authorization': `Bearer ${token}`
-          }
-        });
-        const regex = /<div[^>]+data-lyrics-container="true"[^>]*>[\s\S]*?<\/div>/g;
-        const data = await response.json();
-        const songUrl = data.response.hits[0].result.path;
-        const songPage = await fetch(`https://genius.com${songUrl}`);
-        const html = await songPage.text();
-        let lyrics = html.match(regex);
-        let lyricsText = '';
-        for (let i = 0; i < lyrics.length; i++) {
-            lyrics[i] = lyrics[i].replace(/<(?!br\s*\/?)[^>]+>/g, '');
-            lyricsText += lyrics[i];
-            lyricsText += "<br>";
+  const [songTitle, setSongTitle] = useState('');
+  const [artistName, setArtistName] = useState('');
+  const [lyrics, setLyrics] = useState('');
+  const [missingWords, setMissingWords] = useState([]);
+  const [answers, setAnswers] = useState([]);
+
+  const fetchLyrics = async () => {
+    try {
+      const token = process.env.REACT_APP_GENIUS_TOKEN;
+      const response = await fetch(`https://api.genius.com/search?q=${songTitle} ${artistName}`, {
+        headers: {
+          'Authorization': `Bearer ${token}`
         }
-        setLyrics(lyricsText);
-      } catch (error) {
-        console.error('Error:', error);
-        console.log('Nie udało się pobrać tekstów piosenek.');
+      });
+      const regex = /<div[^>]+data-lyrics-container="true"[^>]*>[\s\S]*?<\/div>/g;
+      const data = await response.json();
+      const songUrl = data.response.hits[0].result.path;
+      const songPage = await fetch(`https://genius.com${songUrl}`);
+      const html = await songPage.text();
+      let lyrics = html.match(regex);
+      let lyricsText = '';
+      for (let i = 0; i < lyrics.length; i++) {
+        lyrics[i] = lyrics[i].replace(/<(?!br\s*\/?)[^>]+>/g, '');
+        lyricsText += lyrics[i];
+        lyricsText += "<br>";
+      }
+      setLyrics(lyricsText);
+    } catch (error) {
+      console.error('Error:', error);
+      console.log('Nie udało się pobrać tekstów piosenek.');
+    }
+  }
+  const extractWords = () => {
+    const words = lyrics.split(' ');
+    const randomIndexes = [];
+    const missingWordsCopy = [];
+
+    // Losujemy indeksy słów do wycięcia
+    while (randomIndexes.length < 10) {
+      const randomIndex = Math.floor(Math.random() * words.length);
+      if (!randomIndexes.includes(randomIndex)) {
+        randomIndexes.push(randomIndex);
       }
     }
-    const extractWords = () => {
-        const words = lyrics.split(' ');
-        const randomIndexes = [];
-        const missingWordsCopy = [];
-  
-        // Losujemy indeksy słów do wycięcia
-        while (randomIndexes.length < 10) {
-          const randomIndex = Math.floor(Math.random() * words.length);
-          if (!randomIndexes.includes(randomIndex)) {
-            randomIndexes.push(randomIndex);
-          }
+
+    // Tworzymy kopię tekstu piosenki z zamienionymi słowami
+    const censoredLyrics = lyrics.split(' ').map((word, index) => {
+      if (randomIndexes.includes(index)) {
+        if (word.length > 1 && !/<[^>]+>/g.test(word) && !word.includes(',') && !/&[#0-9A-Za-z]+;/g.test(word) && !missingWordsCopy.includes(word)) {
+          missingWordsCopy.push(word);
+          return '______';
         }
-  
-        // Tworzymy kopię tekstu piosenki z zamienionymi słowami
-        const censoredLyrics = lyrics.split(' ').map((word, index) => {
-            if (randomIndexes.includes(index)) {
-                if ( word.length > 1 && !/<[^>]+>/g.test(word) && !word.includes(',') && !/&[#0-9A-Za-z]+;/g.test(word) && !missingWordsCopy.includes(word)) {
-                    missingWordsCopy.push(word);
-                    return '______';
-                }
-                else{
-                    let indexOf = randomIndexes.indexOf(index);
-                    randomIndexes.splice(indexOf, 1);
-                }
-            }
-            return word;
-        }).join(' ');
-        console.log(missingWordsCopy)
-        setAnswers(missingWordsCopy);
-        setMissingWords(randomIndexes);
-        setLyrics(censoredLyrics);
-      }
-  
-      const checkAnswer = (userAnswer, e) => {
-        if(!userAnswer) return;
-        for (let index = 0; index < answers.length; index++) {
-            const answer = answers[index];
-            if (userAnswer.toLowerCase() === answer.toLowerCase()) {
-                e.value = '';
-                let lyricsCopy = lyrics.split(' ');
-                lyricsCopy[missingWords[index]] = answer;
-                answers.splice(index, 1);
-                missingWords.splice(index, 1);
-                setAnswers(answers);
-                setMissingWords(missingWords);
-                setLyrics(lyricsCopy.join(' '));
-            };
+        else {
+          let indexOf = randomIndexes.indexOf(index);
+          randomIndexes.splice(indexOf, 1);
         }
       }
-    return (
-        <HomeStyles>
-        <h1>Zgadnij tekst piosenki</h1>
+      return word;
+    }).join(' ');
+    console.log(missingWordsCopy)
+    console.log(randomIndexes)
+    setAnswers(missingWordsCopy);
+    setMissingWords(randomIndexes);
+    setLyrics(censoredLyrics);
+  }
+
+  const checkAnswer = (userAnswer, e) => {
+    if (!userAnswer) return;
+    for (let index = 0; index < answers.length; index++) {
+      const answer = answers[index];
+      if (userAnswer.toLowerCase() === answer.toLowerCase()) {
+        e.value = '';
+        let lyricsCopy = lyrics.split(' ');
+        lyricsCopy[missingWords[index]] = answer;
+        answers.splice(index, 1);
+        missingWords.splice(index, 1);
+        setAnswers(answers);
+        setMissingWords(missingWords);
+        setLyrics(lyricsCopy.join(' '));
+      };
+    }
+  }
+  return (
+    <HomeStyles>
+      <h1>Zgadnij tekst piosenki</h1>
+      <div>
+        <label>Tytuł Piosenki:</label>
+        <input
+          type="text"
+          value={songTitle}
+          onChange={(e) => setSongTitle(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Artysta:</label>
+        <input
+          type="text"
+          value={artistName}
+          onChange={(e) => setArtistName(e.target.value)}
+        />
+      </div>
+      <button onClick={fetchLyrics}>Pobierz Tekst</button>
+      <button onClick={extractWords}>Zacznij grę</button>
+      <div>
+        <h2>Tekst Piosenki:</h2>
+        <div dangerouslySetInnerHTML={{ __html: lyrics }} />
+      </div>
+      {missingWords.length !== 0 && (
         <div>
-          <label>Tytuł Piosenki:</label>
+          <label>Wprowadź brakujące słowa({missingWords.length}):</label>
           <input
             type="text"
-            value={songTitle}
-            onChange={(e) => setSongTitle(e.target.value)}
+            onChange={(e) => checkAnswer(e.target.value, e.target)}
           />
         </div>
-        <div>
-          <label>Artysta:</label>
-          <input
-            type="text"
-            value={artistName}
-            onChange={(e) => setArtistName(e.target.value)}
-          />
-        </div>
-        <button onClick={fetchLyrics}>Pobierz Tekst</button>
-        <button onClick={extractWords}>Zacznij grę</button>
-        <div>
-          <h2>Tekst Piosenki:</h2>
-          <div dangerouslySetInnerHTML={{ __html: lyrics }} />
-        </div>
-        {missingWords.length !== 0 && (
-          <div>
-            <label>Wprowadź brakujące słowa({missingWords.length}):</label>
-            <input
-              type="text"
-              onChange={(e) => checkAnswer(e.target.value, e.target)}
-            />
+      )}
+      {
+        missingWords.length === 0 && (
+          <div className='congrats'>
+            <h2>Gratulacje! Udało Ci się uzupełnić wszystkie luki!</h2>
           </div>
-        )}
-        {
-            missingWords.length === 0 && (
-                <div className='congrats'>
-                    <h2>Gratulacje! Udało Ci się uzupełnić wszystkie luki!</h2>
-                </div>
-            )
-        }
-      </HomeStyles>
-    )
+        )
+      }
+    </HomeStyles>
+  )
 }
